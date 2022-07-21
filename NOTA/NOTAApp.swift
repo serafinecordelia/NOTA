12554f12012fb63d9af53b30f66fdc0d52e51c98
Serafine <serafineecordeliaa@gmail.com>
2022-07-21 23:21:04 +0700

    Initial Commit

---
+++ NOTA/NOTAApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOTAApp.swift
+//  NOTA
+//
+//  Created by Serafine Cordelia on 21/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NOTAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
